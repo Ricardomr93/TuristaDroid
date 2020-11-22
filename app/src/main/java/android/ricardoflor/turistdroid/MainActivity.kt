e97419e20604ad1163806bf8cfcd0922e2f069fb
Ricardo <ricar.mart93@gmail.com>
2020-11-22 17:53:44 +0100

    Creacion del proyecto

---
+++ app/src/main/java/android/ricardoflor/turistdroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package android.ricardoflor.turistdroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

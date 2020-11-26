a106f8de326e80df64da15922ce74245b7b3e02e
Flor Santos <florsantosbueno@gmail.com>
2020-11-26 19:49:25 +0100

    Control de errores

--- app/src/main/java/android/ricardoflor/turistdroid/LoginActivity.kt
+++ app/src/main/java/android/ricardoflor/turistdroid/LoginActivity.kt
@@ -1,11 +1,51 @@
 package android.ricardoflor.turistdroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
+
+    val mail: String = ""
+    val pwd: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+
+
+    }
+
+    //Funcion onClick del botón Login
+    fun entrar(view: View){
+
+        //Condicion que comprueba el correo
+        if (isMailValid(editTextLoginMail.text.toString())){
+            editTextLoginMail.error = resources.getString(R.string.email_incorrecto)
+        }
+
+        if (!isPasswordValid(editTextLoginPassword.text.toString())){
+            editTextLoginPassword.error = resources.getString(R.string.pwd_incorrecto)
+        }
+
+    }
+
+    // Metodo para validar el EMAIL
+    private fun isMailValid(mail: String): Boolean {
+        return if (mail.contains('@')) {
+            Patterns.EMAIL_ADDRESS.matcher(mail).matches()
+        } else {
+            mail.isNotBlank()
+        }
+    }
+
+    // Metodo para validar la PASSWORD
+    private fun isPasswordValid(password: String): Boolean {
+        return password.length > 5
     }
 }
